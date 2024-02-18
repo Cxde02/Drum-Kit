c5cb99137efb37916572dbaa85509c8ae12a0087
Ruhomaun Khaleelullah Ahmad Raza Qadri <105217847+Cxde02@users.noreply.github.com>
2024-02-18 21:00:15 +0400

    Added js

---
+++ javascript/script.js
@@ -0,0 +1,26 @@
+window.addEventListener("keydown", function (e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) {
+    return; // stop the function from running all together
+  } else {
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add("playing");
+    //key.classList.remove("playing");
+    //key.classList.toggle("playing");
+    //setTimeout(function () {}, 0.07);
+  }
+});
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") {
+    return; //skip if not a transform
+  } else {
+    this.classList.remove("playing");
+  }
+}
+
+const keys = document.querySelectorAll(".key");
+//keys.addEventListener('transitionend');
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
